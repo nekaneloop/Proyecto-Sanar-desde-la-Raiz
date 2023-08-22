71edc62555b2db0fe395b269c3e54727fe760b35
nekaneloop <gonzaleslop03@gmail.com>
2023-08-22 10:31:38 -0500

    ADD: Header buttons

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+function Header() {
+    return (
+
+        <div>
+            <button className="btn-home">
+                Home
+            </button>
+
+            <button className="btn-aboutMe">
+                Sobre mí
+            </button>
+
+            <button className="btn-services">
+                Servicios
+            </button>
+
+            <button className="btn-date">
+                Separa tu cita aquí
+            </button>
+        </div>
+    )
+}
+
+export default Header;
